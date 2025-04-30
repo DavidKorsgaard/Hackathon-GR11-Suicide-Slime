8177a8d0de840f24ddffa1407a0c3f83570c9423
BertIsTheWurd <bertram.pinstrup.jensen@outlook.com>
2025-04-30 11:01:52 +0200

    Funky slime script
    Guys who's just heard of modulo:

--- Suicide Slime/Assets/Scripts/Slime.cs
+++ Suicide Slime/Assets/Scripts/Slime.cs
@@ -4,13 +4,43 @@ public class Slime : MonoBehaviour
 {
     private new Renderer renderer;
 
+    //Mig der prøver at finde på det modsatte af hunger :(
+    [SerializeField] private int maxSatiety;
+    private int satiety;
+    [SerializeField] private float hungerRate =1;
+    private float hungerTime;
+    
     void Start()
     {
         renderer = GetComponent<Renderer>();
+        satiety = maxSatiety;
     }
 
-    public void ChangeColor(Color color)
+    void Update()
     {
+        hungerTime += Time.deltaTime;
+        if (hungerTime % hungerRate >= 1)
+        {
+            hungerTime -= hungerRate;
+            satiety--;
+            Debug.Log("Satiety: " + satiety);
+        }
+
+        if (satiety <= 0)
+        {
+            //Run some kill slime method elsewhere
+            #if DEBUG
+            Debug.Log("Slime dead");
+            #endif
+            
+        }
+    }
+
+    public void FeedSlime(Color color, int nutrition)
+    {
+        Debug.Log("Feed Slime!");
+        satiety += nutrition;
+        Debug.Log("New Satiety: " + satiety);
         renderer.material.color = color;
     }
 }
